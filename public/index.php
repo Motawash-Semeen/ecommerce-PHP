a4cae1fb03c0f5e12968a7fad580f6c4d4bc2b34
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-05-08 00:04:09 +0600

    init

---
+++ public/index.php
@@ -0,0 +1,70 @@
+<?php
+include("./includes/header.php");
+?>
+
+<!-- Topbar Start -->
+<?php
+include("./includes/topbar.php");
+?>
+
+<!-- Topbar End -->
+
+
+<!-- Navbar Start -->
+<?php
+include("./includes/navbar.php");
+?>
+
+<!-- Navbar End -->
+
+
+<!-- Carousel Start -->
+<?php
+include("./includes/carousel.php");
+?>
+<!-- Carousel End -->
+
+
+<!-- Categories Start -->
+
+<?php
+include("./includes/categories.php");
+?>
+
+<!-- Categories End -->
+
+
+<!-- Products Start -->
+<?php
+include("./includes/featured.php");
+?>
+
+<!-- Products End -->
+
+
+<!-- Offer Start -->
+<?php
+include("./includes/offer.php");
+?>
+<!-- Offer End -->
+
+
+<!-- Products Start -->
+<?php
+include("./includes/recent.php");
+?>
+<!-- Products End -->
+
+
+<!-- Vendor Start -->
+<?php
+include("./includes/vendor.php");
+?>
+
+<!-- Vendor End -->
+
+<!-- Footer Start -->
+<?php
+include("./includes/footer.php");
+?>
+<!-- Footer End -->
