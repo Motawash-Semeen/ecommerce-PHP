afcac205adb5817203585fd299dee2fe68c6e06c
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2023-05-13 00:42:14 +0600

    profile

--- public/admin/includes/navigation.php
+++ public/admin/includes/navigation.php
@@ -17,6 +17,9 @@
 
                 <li class="divider"></li>
                 <li>
+                    <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile </a>
+                </li>
+                <li>
                     <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                 </li>
             </ul>
